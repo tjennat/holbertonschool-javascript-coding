4d59e0cf25298e7ca482e8e205d9fd2b57021a1c
tjennat <theo.jennat98@gmail.com>
2024-05-14 16:57:10 +0200

    another attempt with a different way

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-function writeFile (filePath, content) {
-  fs.writeFile(filePath, content, 'utf-8', (err) => {
-    if (err) {
-    console.error(err);
-    }
-  });
+if (process.argv.length !== 4) {
+  process.exit(1);
 }
 
-const filePath = '/path/to/file.txt';
-const content = 'Hello, world!';
+const filePath = process.argv[2];
+const content = process.argv[3];
 
-writeFile(filePath, content);
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
